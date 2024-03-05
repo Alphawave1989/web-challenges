d25bf99760fd78e218f0e7fd99bbf6d3a253958b
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-03-05 12:50:02 +0100

    done

--- nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
+++ nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
@@ -1,8 +1,20 @@
 import Link from "next/link";
 import { introduction } from "../../lib/data";
 import { volumes } from "../../lib/data";
+import { useRouter } from "next/router";
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+console.log(getRandomElement(volumes));
 
 export default function Volumes() {
+  const router = useRouter(); // Klammern vergessen
+  function handleRandomVolume(event) {
+    const randomVolume = getRandomElement(volumes); // const hatte ich vergessen
+    router.push(`/volumes/${randomVolume.slug}`);
+  }
+
   return (
     <>
       <h1>List of Volumes</h1>
@@ -15,6 +27,7 @@ export default function Volumes() {
           );
         })}
       </ul>
+      <button onClick={handleRandomVolume}>Go to Random Volume</button>
     </>
   );
 }
