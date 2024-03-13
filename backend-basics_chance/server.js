b495b6c24073d63a05da5413b5283efa88fc044e
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-03-13 11:29:14 +0100

    done

--- backend-basics_chance/server.js
+++ backend-basics_chance/server.js
@@ -9,6 +9,6 @@ export const server = createServer((request, response) => {
   const profession = chance.profession();
   response.statusCode = 200;
   response.end(
-    `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+    `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}. `
   );
 });
