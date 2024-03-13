e933214d970cc96176e1d2eb701150885cbfa7ab
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-03-13 11:28:34 +0100

    done

--- backend-basics_chance/index.js
+++ backend-basics_chance/index.js
@@ -1,4 +1,7 @@
 import { server } from "./server.js";
 
 const port = 3000;
-server.listen(port, () => console.log(`Server is running on ${port}`));
+
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
